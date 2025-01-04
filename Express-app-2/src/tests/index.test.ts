0b90c51521d21db4804194ce44fe17921349aebe
TheMikeKaisen <h.karthiknair@gmail.com>
2025-01-04 09:17:18 +0530

    test an express app without db conn.

---
+++ Express-app-2/src/tests/index.test.ts
@@ -0,0 +1,34 @@
+import {describe, it, expect} from '@jest/globals'
+import {app} from '../index'
+import request from 'supertest';
+
+
+describe("testing POST/sum", () => {
+    it("should return the sum of two numbers.", async () => {
+        const res = await request(app).post("/sum").send({
+            a:1, 
+            b:2
+        })
+
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(3);
+    })
+    it("should return the sum of two negative numbers", async () => {
+        const res = await request(app).post("/sum").send({
+          a: -1,
+          b: -2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(-3);
+      });
+
+      it("should return the sum of two zero number", async () => {
+        const res = await request(app).post("/sum").send({
+          a: 0,
+          b: 0
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(0);
+      });
+})
+
